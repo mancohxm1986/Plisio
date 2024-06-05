00845360639da45e3d01a5819a15abcd3a5a9ce8
mancohxm1986 <chunsong1986@gmail.com>
2024-06-05 20:48:51 +0800

    Initial commit of Plisio payment gateway plugin

---
+++ assets/language.php
@@ -0,0 +1,8 @@
+<?php
+$text_title                 = 'Pay with Cryptocurrency';
+$button_confirm             = 'Pay with Plisio';
+$button_confirm_white_label = 'Pay';
+$button_currency_confirm    = 'Pay %s with Plisio';
+$pay_with_text              = 'Pay with: ';
+$text_checkout              = 'Checkout';
+?>
